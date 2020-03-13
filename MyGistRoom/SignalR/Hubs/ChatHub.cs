1a96183a3e7750cef4e07fdd3518e1c2f6088cfc
prodigy4801 <prodigy4801@gmail.com>
2020-03-13 13:28:01 +0100

    initial commit - SignalR is set up but still in development... :(

---
+++ MyGistRoom/SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MyGistRoom.SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        //public void Hello()
+        //{
+        //    Clients.All.hello();
+        //}
+
+        public void Send(string name, string message)
+        {
+            Clients.All.AddNewMessageToPage(name, message);
+        }
+    }
+}
